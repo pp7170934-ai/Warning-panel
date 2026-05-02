4f1eb49d200b7122550aa1732159c7ea9f5ce34b
peterthecoolest21-svg <peterthecoolest21@gmail.com>
2026-05-02 23:00:16 +0800

    Add files via upload

--- supabase-config.js
+++ supabase-config.js
@@ -2,5 +2,5 @@
 // Paste your Supabase values below.
 // You can find them in Supabase Dashboard → Project Settings → API.
 
-const SUPABASE_URL = "const SUPABASE_URL = "https://kizcxngtiwslthgvylfm.supabase.co";";
-const SUPABASE_ANON_KEY = "sb_publishable_REFyXsrzuiK19kfoVX0XaQ_qRnqYcyc";
+window.SUPABASE_URL = "https://kizcxngtiwslthgvylfm.supabase.co";
+window.SUPABASE_ANON_KEY = "sb_publishable_REFyXsrzuiK19kfoVX0XaQ_qRnqYcyc";
