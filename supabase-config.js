2c843200324a22482dec8a8232462305fa42a808
GoofyStudio <peterthecoolest21@gmail.com>
2026-05-03 20:17:05 +0800

    Add files via upload

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,2 @@
-// Rename this file to supabase-config.js if needed.
-// Paste your Supabase values below.
-// You can find them in Supabase Dashboard → Project Settings → API.
-
 window.SUPABASE_URL = "https://kizcxngtiwslthgvylfm.supabase.co";
 window.SUPABASE_ANON_KEY = "sb_publishable_REFyXsrzuiK19kfoVX0XaQ_qRnqYcyc";
